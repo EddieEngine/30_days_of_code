718de21b313f4e08f5130794bc838adace88b320
hernevans <31022697+Hernevans@users.noreply.github.com>
2022-06-06 14:19:19 +0100

    upload .py file for day_0 task

---
+++ Day_0/solution.py
@@ -0,0 +1,8 @@
+input_string = input()
+
+# Prints a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# Prints the contents of input_string to stdout.
+print(input_string)
+
