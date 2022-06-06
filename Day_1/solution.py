b71f46fe9c707f2e0df1bfa2b544ef09ef41fc21
hernevans <31022697+Hernevans@users.noreply.github.com>
2022-06-06 16:10:10 +0100

    solved task 1 of challenge

---
+++ Day_1/solution.py
@@ -0,0 +1,18 @@
+# Initialize variables i, d and s
+i = 4
+d = 4.0
+s = 'HackerRank'
+
+# Declaring, reading and saving second integer, double amd String vars
+i_2 = int(input('Enter integer: '))
+d_2 = float(input('Enter float: '))
+s_2 = str(input('Enter string: '))
+
+# Print the sum of both integer vars
+print(i+i_2)
+
+# Print the sum of the double vars
+print(d+d_2)
+
+# Concatenate the string vars
+print(s+s_2)
